13238e04c2a90d416052ff58f1af70eee36280b4
Stephy Arun <arunma53@gmail.com>
2024-04-29 08:53:10 +0000

    second

--- models/index.js
+++ models/index.js
@@ -1,1 +1,23 @@
-//in the bookshelf  orm(and mo)git
+//in the bookshelf  orm(and most ORM ), a model
+// represent on Table in you databasae
+//you issue coomands (in the js) on the model, and the model
+//translate your commands to sql(or whatever DB you are using)
+
+//when you requuire a file , and its a js file, you can omit the extention
+//instead of const bookshelf = require('../bookshelf/index.js');
+//=> const bookshelf  = require('../bookshelf/index)
+//when you require a file in the folder and the file is index
+//you can omit the 'index'(you can omit the filename)
+
+const bookshelf = require('../bookshelf');
+
+//create a product model
+//one model represent one table in your database
+//first argument is the name of your model
+//second argument is a configure obj
+
+const Product = bookshelf.model('Product',{
+   tableName:'products'
+})
+
+module.exports = {Product}
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const router = express.Router(); // #1 - Create a new express Router
 
+//require in the model
+const { Product } = require('../models')
+
 
 /// a router object can contain routes
-router.get('/', (req,res)=>{
-    res.send("All products")
-})
+router.get('/', async function(req,res){
+   // use the product model to get all products
+   const products = await Product.collection().fetch();
+   //products.tojSON() convert the table rows  into JSON format
+   res.render('products/index',{
+    products:products.toJSON()
+   });
+});
 
  router.get('/add-products',function(req,res){
     res.send("Add product")
