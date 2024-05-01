dcf8283aedb09574a7a0a879adc830088447b3a3
Stephy Arun <arunma53@gmail.com>
2024-05-01 03:11:10 +0000

    seven

--- migrations/20240501000643-add-fk-to-products.js
+++ migrations/20240501000643-add-fk-to-products.js
@@ -18,14 +18,20 @@ exports.up = function(db) {
   //first parameter: the table which the fk is supposed to be in
   //second parameter : the table which the fk is referencing to
   //third paramter : object. the key is the fk and the vakue is what it is supposed to point to
-
-  return db.addForeignKey('products','categories',{
+  //fourth parameter: the name of the foreign key
+  //fifth parameter:FK rules
+  return db.addForeignKey('products','categories','product_category_fk',{
     'category_id':'id'
+  },{
+    onDelete:"CASCADE", // onDelete : what to do if a category is deleted
+    onUpdate:"RESTRICT" //
   });
 };
 
 exports.down = function(db) {
-  return null;
+  // agrument 1 : table to remove from
+  //argument 2 ; the name of the foreign key
+  return db.removeForeignKey('products','product_category_fk');
 };
 
 exports._meta = {
